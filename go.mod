2ff7cf5317cff5bab62c6272c9d1e8f70803d9ce
michelle jae <moyer.mj@gmail.com>
2022-03-10 16:02:49 -0500

    templating working

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/michellejae/coned
 
 go 1.17
 
-require (
-	github.com/go-chi/chi/v5 v5.0.7
-	github.com/go-echarts/go-echarts/v2 v2.2.4
-)
+require github.com/go-echarts/go-echarts/v2 v2.2.4
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +14,8 @@ import (
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
+
+	chartrender "github.com/go-echarts/go-echarts/v2/render"
 )
 
 const (
@@ -37,6 +42,7 @@ type Energy struct {
 }
 
 type HomePage struct {
+	HTML template.HTML
 }
 
 func newEnergy(name, offerType, energySource, percentRenew, cancellation string, rate, term float64) *Energy {
@@ -79,6 +85,7 @@ func main() {
 	calculateDecTotal(source)
 
 	http.HandleFunc("/", generateAndGraph)
+	fmt.Println("Starting the server on :3334")
 	http.ListenAndServe(":3334", nil)
 
 }
@@ -132,6 +139,11 @@ func calculateDecTotal(source []*Energy) {
 }
 
 func generateAndGraph(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("html/giraffe.html")
+	if err != nil {
+		panic(err)
+	}
 
 	options := opts.BarData{}
 
@@ -189,68 +201,78 @@ func generateAndGraph(w http.ResponseWriter, r *http.Request) {
 			Height: "600px",
 		}))
 
-	bar.Render(w)
+	bar.Renderer = newSnippetRenderer(bar, bar.Validate)
+	var htmlSnippet template.HTML = renderToHtml(bar)
+
+	tmplData := HomePage{
+		HTML: htmlSnippet,
+	}
+
+	err = t.Execute(w, tmplData)
+	if err != nil {
+		panic(err)
+	}
 }
 
-// func generateAndGraph(source []*Energy) {
+type Renderer interface {
+	Render(w io.Writer) error
+}
 
-// 	options := opts.BarData{}
+var baseTpl = `
+<div class="container">
+    <div class="item" id="{{ .ChartID }}" style="width:{{ .Initialization.Width }};height:{{ .Initialization.Height }};"></div>
+</div>
+{{- range .JSAssets.Values }}
+   <script src="{{ . }}"></script>
+{{- end }}
+<script type="text/javascript">
+    "use strict";
+    let goecharts_{{ .ChartID | safeJS }} = echarts.init(document.getElementById('{{ .ChartID | safeJS }}'), "{{ .Theme }}");
+    let option_{{ .ChartID | safeJS }} = {{ .JSON }};
+    goecharts_{{ .ChartID | safeJS }}.setOption(option_{{ .ChartID | safeJS }});
+    {{- range .JSFunctions.Fns }}
+    {{ . | safeJS }}
+    {{- end }}
+</script>
+`
 
-// 	data := make([]opts.BarData, 0)
+type snippetRenderer struct {
+	c      interface{}
+	before []func()
+}
 
-// 	for _, val := range source {
+func newSnippetRenderer(c interface{}, before ...func()) chartrender.Renderer {
+	return &snippetRenderer{c: c, before: before}
+}
 
-// 		percentFloat, _ := strconv.ParseFloat(val.PercentRenew, 64)
-// 		percentFloat = percentFloat * 100
+func (r *snippetRenderer) Render(w io.Writer) error {
+	const tplName = "chart"
+	for _, fn := range r.before {
+		fn()
+	}
 
-// 		// have to declare these inside the range so they each update for every ESCO
-// 		// i'm not sure why, something with pointer?
-// 		toolTip := opts.Tooltip{}
-// 		itemStyle := opts.ItemStyle{}
+	tpl := template.
+		Must(template.New(tplName).
+			Funcs(template.FuncMap{
+				"safeJS": func(s interface{}) template.JS {
+					return template.JS(fmt.Sprint(s))
+				},
+			}).
+			Parse(baseTpl),
+		)
 
-// 		options.Name = val.Name
-// 		options.Value = val.Total
+	err := tpl.ExecuteTemplate(w, tplName, r.c)
+	return err
+}
 
-// 		toolTip.Show = true
-// 		toolTip.Formatter = fmt.Sprintf("Name: %v<br />Total: $%v<br />Rate: %.2f ¢/kWh<br />Offer Type: %v<br />Minimum Contract Length: %v months<br />Energy Source: %v<br />Percent Renewable: %v%%",
-// 			val.Name, val.Total, val.Rate, val.OfferType, val.MinTerm, val.EnergySource, percentFloat)
+func renderToHtml(c interface{}) template.HTML {
+	var buf bytes.Buffer
+	r := c.(chartrender.Renderer)
+	err := r.Render(&buf)
+	if err != nil {
+		log.Printf("Failed to render chart: %s", err)
+		return ""
+	}
 
-// 		options.Tooltip = &toolTip
-
-// 		if val.Name == "Consolidated Edison Company of New York, Inc." {
-// 			itemStyle.Color = "red"
-// 			options.ItemStyle = &itemStyle
-
-// 		} else {
-// 			itemStyle.Color = "green"
-// 			options.ItemStyle = &itemStyle
-
-// 		}
-
-// 		data = append(data, options)
-
-// 	}
-
-// 	bar := charts.NewBar()
-
-// 	bar.AddSeries("Totals", data)
-
-// 	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
-// 		Title:    "My Dec 2021 Energy Bills per ESCO",
-// 		Subtitle: "ConEd Delivery Rate + (ESCO rate * kw usage)",
-// 	}),
-// 		charts.WithXAxisOpts(opts.XAxis{
-// 			Type: "category",
-// 			Show: false,
-// 			Name: "ESCO's",
-// 		}),
-// 		//charts.WithTooltipOpts(opts.Tooltip{Show: true}),
-// 		charts.WithInitializationOpts(opts.Initialization{
-// 			Width:  "1200px",
-// 			Height: "600px",
-// 		}))
-// 	f, _ := os.Create("index.html")
-
-// 	bar.Render(f)
-
-// }
+	return template.HTML(buf.String())
+}
